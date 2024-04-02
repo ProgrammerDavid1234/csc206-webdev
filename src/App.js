f5debb6aff5424e44fb323c523f30c52e85ed03e
ProgrammerDavid1234 <olonadenifemi@gmail.com>
2024-04-02 15:23:19 +0100

    userInterface

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ textAlign: 'center' }}>CSC 206</h1>
+      <div className="card-container">
+        <div className="card">
+          <h2>Card 1 Title</h2>
+          <p>Some context for Card 1.</p>
+          <button>Learn More</button>
+        </div>
+        <div className="card">
+          <h2>Card 2 Title</h2>
+          <p>Some context for Card 2.</p>
+          <button>Learn More</button>
+        </div>
+        <div className="card">
+          <h2>Card 3 Title</h2>
+          <p>Some context for Card 3.</p>
+          <button>Learn More</button>
+        </div>
+        <div className="card">
+          <h2>Card 4 Title</h2>
+          <p>Some context for Card 4.</p>
+          <button>Learn More</button>
+        </div>
+        <div className="card">
+          <h2>Card 4 Title</h2>
+          <p>Some context for Card 4.</p>
+          <button>Learn More</button>
+        </div>
+        <div className="card">
+          <h2>Card 4 Title</h2>
+          <p>Some context for Card 4.</p>
+          <button>Learn More</button>
+        </div>
+        
+      </div>
     </div>
   );
 }
